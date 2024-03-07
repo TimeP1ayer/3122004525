452fdb53fbc372b3e6beb9217a7c2823fe160dc0
TimeP1ayer <1319683633@qq.com>
2024-03-07 19:35:38 +0800

    完成txt文档读取为str的模块

---
+++ src/entrance.java
@@ -0,0 +1,6 @@
+public class entrance {
+    public static void main(String[] args) {
+        String str = readtext.readtxt("D:\\Users\\Desktop\\orig.txt");
+        System.out.println(str);
+    }
+}
